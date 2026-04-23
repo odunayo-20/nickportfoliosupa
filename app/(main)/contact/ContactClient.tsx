7cdeecc9580129f0a9ed02d51a51ba8d4df5fe92
mecbilltech <mecbilltech@gmail.com>
2026-04-23 18:42:15 +0100

    little changes

--- app/(main)/contact/ContactClient.tsx
+++ app/(main)/contact/ContactClient.tsx
@@ -363,14 +363,14 @@ const ContactClient = () => {
             {/* Grayscale Map Wrapper */}
             <div className="absolute inset-0 grayscale contrast-[1.1] brightness-[0.95] opacity-90 transition-all duration-700 group-hover:grayscale-0 group-hover:contrast-100 group-hover:brightness-100">
               <iframe 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23157.65991873138!2d15.225950854443358!3d44.11936354898114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4761fa6627038e87%3A0x400ad50862bd150!2sZadar%2C%20Croatia!5e0!3m2!1sen!2suk!4v1713891234567" 
+                src="https://maps.google.com/maps?q=Zadar,%20Croatia&t=&z=13&ie=UTF8&iwloc=&output=embed" 
                 width="100%" 
                 height="100%" 
                 style={{ border: 0 }} 
                 allowFullScreen={true} 
                 loading="lazy" 
                 referrerPolicy="no-referrer-when-downgrade"
-                className="grayscale"
+                className=""
               />
             </div>
 
